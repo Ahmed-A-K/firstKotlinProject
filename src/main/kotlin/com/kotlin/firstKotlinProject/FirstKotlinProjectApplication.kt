e6f6019e42b67e52ad4810b6af25a94fd017fe3b
Ahmed Ali Khan <ahmedalikhan652@gmail.com>
2023-11-01 12:30:46 +0500

    Initial commit

---
+++ src/main/kotlin/com/kotlin/firstKotlinProject/FirstKotlinProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.firstKotlinProject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstKotlinProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstKotlinProjectApplication>(*args)
+}
